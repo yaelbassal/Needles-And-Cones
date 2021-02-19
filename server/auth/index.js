809176c2284c58aa546655a660c31eb5295425a7
Yael Bassal <ybassal@hotmail.com>
2021-02-19 10:53:03 -0500

    form fields in signup component updated and data successfully getting added to user db model

--- server/auth/index.js
+++ server/auth/index.js
@@ -21,7 +21,11 @@ router.post('/login', async (req, res, next) => {
 
 router.post('/signup', async (req, res, next) => {
   try {
-    const user = await User.create(req.body)
+    const user = await User.create({
+      name: req.body.fullName,
+      email: req.body.email,
+      password: req.body.password
+    })
     req.login(user, err => (err ? next(err) : res.json(user)))
   } catch (err) {
     if (err.name === 'SequelizeUniqueConstraintError') {
