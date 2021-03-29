e6c1b485cf42905d1230c23c7796620fe0303b3d
Yael Bassal <ybassal@hotmail.com>
2021-03-29 15:05:55 -0400

    deployment config variable updated

--- server/db/db.js
+++ server/db/db.js
@@ -3,6 +3,24 @@ const pkg = require('../../package.json')
 
 const databaseName = pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '')
 
+let config
+if (process.env.DATABASE_URL) {
+  config = {
+    logging: false, //put true if you like the logs
+    ssl: true,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+  }
+} else {
+  config = {
+    logging: false
+  }
+}
+
 const db = new Sequelize(
   process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
   {
