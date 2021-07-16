a26e614f76f173f8296a057ca8d22a8bdae14bb2
Yael Bassal <ybassal@hotmail.com>
2021-07-16 12:35:02 -0400

    styling in progress....

--- client/components/find.js
+++ client/components/find.js
@@ -3,10 +3,26 @@ import {connect} from 'react-redux'
 import Tree from 'react-d3-tree'
 import {pineIdChart} from '../config'
 
+// Here we're using `renderCustomNodeElement` to represent each node
+// as an SVG `rect` instead of the default `circle`.The <rect> element is a basic SVG shape that draws rectangles, defined by their position, width, and height. The rectangles may have their corners rounded.
+const renderRectSvgNode = ({nodeDatum, toggleNode, foreignObjectProps}) => (
+  <g>
+    <rect width="30" height="30" x="-10" onClick={toggleNode} fill="white" />
+    <foreignObject {...foreignObjectProps}>
+      <div style={{border: '1px solid black', backgroundColor: '#dedede'}}>
+        <h3 style={{textAlign: 'center'}}>{nodeDatum.name}</h3>
+        {nodeDatum.attributes ? <h3>{nodeDatum.attributes.result}</h3> : <h3 />}
+      </div>
+    </foreignObject>
+  </g>
+)
+
 /**
  * COMPONENT
  */
 export const Find = () => {
+  const nodeSize = {x: 200, y: 200}
+  const foreignObjectProps = {width: nodeSize.x, height: nodeSize.y, x: 20}
   return (
     <div>
       <h2>Identify Your Pine</h2>
@@ -16,9 +32,14 @@ export const Find = () => {
         <Tree
           data={pineIdChart}
           orientation="vertical"
-          pathFunc="step"
+          pathFunc="diagonal"
+          separation={{nonSiblings: 3, siblings: 2}}
           translate={{x: 800, y: 100}}
-          scaleExtent={{min: 0.1, max: 1}}
+          scaleExtent={{min: 0.7, max: 0.7}}
+          renderCustomNodeElement={rd3tProps =>
+            renderRectSvgNode({...rd3tProps, foreignObjectProps})
+          }
+          depthFactor="200"
         />
       </div>
     </div>
