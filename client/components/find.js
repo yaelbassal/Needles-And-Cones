0eaebd36f12d90a5980e758b11ec07e9f1978bf5
Yael Bassal <ybassal@hotmail.com>
2021-07-18 10:25:48 -0400

    styled individual nodes

--- client/components/find.js
+++ client/components/find.js
@@ -5,13 +5,20 @@ import {pineIdChart} from '../config'
 
 // Here we're using `renderCustomNodeElement` to represent each node
 // as an SVG `rect` instead of the default `circle`.The <rect> element is a basic SVG shape that draws rectangles, defined by their position, width, and height. The rectangles may have their corners rounded.
+
 const renderRectSvgNode = ({nodeDatum, toggleNode, foreignObjectProps}) => (
   <g>
-    <rect width="30" height="30" x="-10" onClick={toggleNode} fill="white" />
+    <rect
+      width="30"
+      height="30"
+      x="-20"
+      onClick={toggleNode}
+      fill="chocolate"
+    />
     <foreignObject {...foreignObjectProps}>
-      <div style={{border: '1px solid black', backgroundColor: '#dedede'}}>
-        <h3 style={{textAlign: 'center'}}>{nodeDatum.name}</h3>
-        {nodeDatum.attributes ? <h3>{nodeDatum.attributes.result}</h3> : <h3 />}
+      <div className="foreign-object">
+        <h3>{nodeDatum.name}</h3>
+        {nodeDatum.attributes ? <h3>{nodeDatum.attributes.result}</h3> : <br />}
       </div>
     </foreignObject>
   </g>
@@ -20,19 +27,19 @@ const renderRectSvgNode = ({nodeDatum, toggleNode, foreignObjectProps}) => (
 /**
  * COMPONENT
  */
+//up to here for styling updates.
 export const Find = () => {
   const nodeSize = {x: 200, y: 200}
   const foreignObjectProps = {width: nodeSize.x, height: nodeSize.y, x: 20}
   return (
-    <div>
-      <h2>Identify Your Pine</h2>
+    <div className="find-container">
+      <h1>Identify Your Pine</h1>
       <div id="treeWrapper">
         {/* https://www.npmjs.com/package/react-d3-tree#customizing-the-tree
       note: use https://github.com/bkrem/react-d3-tree */}
         <Tree
           data={pineIdChart}
           orientation="vertical"
-          pathFunc="diagonal"
           separation={{nonSiblings: 3, siblings: 2}}
           translate={{x: 800, y: 100}}
           scaleExtent={{min: 0.7, max: 0.7}}
